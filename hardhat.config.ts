e466ec01fb092c7b25de134a5988ee2ab0e28f7b
William Cisotto <willgalindo@gmail.com>
2023-04-25 16:50:25 +0100

    renaming test to tests

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,12 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+require("@nomiclabs/hardhat-ethers")
+require("@openzeppelin/hardhat-upgrades")
+
 const config: HardhatUserConfig = {
   solidity: "0.8.18",
+  paths: { tests: "tests" }
 };
 
 export default config;
